6d59921cdf1d7f435f98500d0969fbd7ab0ced56
Amey24S <98271058+Amey24S@users.noreply.github.com>
2022-03-16 12:12:19 +0530

    Add files via upload

---
+++ file_thread.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+void th_fn(void *p);
+int fd;
+
+int main()
+{
+pthread_t t1, t2;
+
+fd = open("/etc/passwd", O_RDONLY);
+printf("Opened with fd: %d\n", fd);
+
+pthread_create(&t1, NULL, th_fn, "Thread1");
+pthread_create(&t2, NULL, th_fn, "Thread2");
+
+pthread_join(t1, NULL);
+pthread_join(t2, NULL);
+
+}
+
+void th_fn(void *p)
+{
+char *str, buff[100];
+int n, pid;
+str = (char *)p;
+pid = getpid();
+printf("%s: \t Started: \t for process: %d \n\n", str,pid);
+do
+{
+n = read(fd,buff,100);
+printf("%s: \t Read: \t %d \n\n", str,pid);
+printf("\n------\n");
+
+write(1,buff,n);
+printf("\n------\n");
+sleep(3);
+}while(n);
+printf("%s: \t Finished: \t For process: %d \n\n", str,pid);
+}
